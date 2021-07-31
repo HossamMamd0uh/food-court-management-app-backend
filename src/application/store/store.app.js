9c4af18adb3e1bfd77c3082963b4c82163a699bb
LAPTOP-1CACDRFD\Hossam Mamdouh <hossammamdouh8010@gmail.com>
2021-07-31 12:52:25 +0200

    get all stores endpoint

--- src/application/store/store.app.js
+++ src/application/store/store.app.js
@@ -2,12 +2,14 @@ import storeService from '../../services/store.service';
 import storeModel from '../../models/store/store';
 
 import createNewStore from './create-new-store/create-new-store';
+import getAllStores from './get-all-stores/get-all-stores';
 // Construct the store service!
 const service = new storeService(new storeModel().constructModel() );
 
 export default () => {
     return Object.freeze({
         createNewStore: new createNewStore(service),
+        getAllStores: new getAllStores(service),
     })
 };
 
