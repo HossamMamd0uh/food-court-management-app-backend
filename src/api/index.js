a13d2fddac1f6a0a1e86aa44c32b6cd6061755ae
LAPTOP-1CACDRFD\Hossam Mamdouh <hossammamdouh8010@gmail.com>
2021-08-04 16:32:42 +0200

    swagger full APIs documentation

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import * as api from './controllers';
 
+// Swaggar call
+
+const swaggerUI = require('swagger-ui-express')
+const swaggerDocument = require('../common/swagger-data/swagger.json')
+
 // Define routes..
 var router = Router();
 
 // API Main route..
 router.use('/api/v1' , api.default);
 
-
+// Swagger route
+router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 export default router;
